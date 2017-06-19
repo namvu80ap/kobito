1e668d8f51d06ed965ef49419ca2dc7ababf442c
Administrator <admin@example.com>
2017-06-19 19:39:12 +0900

    Open market project

--- kobito-ai/src/main/java/com/org/kobito/ai/KobitoAiApplication.java
+++ kobito-ai/src/main/java/com/org/kobito/ai/KobitoAiApplication.java
@@ -2,6 +2,10 @@ package com.org.kobito.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
 
 @SpringBootApplication
 public class KobitoAiApplication {
--- kobito-ai/src/test/java/com/org/kobito/ai/KobitoAiApplicationTests.java
+++ kobito-ai/src/test/java/com/org/kobito/ai/KobitoAiApplicationTests.java
@@ -4,13 +4,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.tensorflow.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class KobitoAiApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		try (Graph g = new Graph()) {
+			final String value = "Hello from " + TensorFlow.version();
+
+			// Construct the computation graph with a single operation, a constant
+			// named "MyConst" with a value "value".
+			try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+				// The Java API doesn't yet include convenience functions for adding operations.
+				g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+			}
+
+			// Execute the "MyConst" operation in a Session.
+			try (Session s = new Session(g);
+				 Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+				System.out.println(new String(output.bytesValue(), "UTF-8"));
+			}
+		}
+
 	}
 
 }
