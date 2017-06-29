bafdfc365997ceda420d9b79780b81439b4e4318
Administrator <admin@example.com>
2017-06-29 14:39:03 +0900

    Update build travis-ci

--- kobito-integration/src/test/java/com/org/kobito/integration/KobitoIntegrationApplicationTests.java
+++ kobito-integration/src/test/java/com/org/kobito/integration/KobitoIntegrationApplicationTests.java
@@ -1,13 +1,11 @@
 package com.org.kobito.integration;
 
-import com.org.kobito.integration.model.TradeTweet;
 import com.org.kobito.integration.services.TradeTweetService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import reactor.core.publisher.Flux;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
