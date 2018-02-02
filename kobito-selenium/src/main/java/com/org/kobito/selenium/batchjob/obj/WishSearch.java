873e3224d07747629910c9b35aa35b2324d8800b
DESKTOP-TUUPH0R\nam <nam@lisuto.com>
2018-02-02 19:44:34 +0900

    Add mongodb

---
+++ kobito-selenium/src/main/java/com/org/kobito/selenium/batchjob/obj/WishSearch.java
@@ -0,0 +1,22 @@
+package com.org.kobito.selenium.batchjob.obj;
+
+public class WishSearch {
+    private Integer marketType;
+    private String  param;
+
+    public Integer getMarketType() {
+        return marketType;
+    }
+
+    public void setMarketType(Integer marketType) {
+        this.marketType = marketType;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+}
